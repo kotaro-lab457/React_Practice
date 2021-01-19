16a6bce70fcfc3130abb25bdb4a4b9dd471d3336
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-01-19 23:39:50 +0900

    useReducer 1/19

--- src/Effect.js
+++ src/Effect.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react'
 
+
+// データ取得、　購読の設定、　DOMのp手動での変更　＝　副作用
+
 const Effect = () => {
     const [count, setCount] = useState(0)
+
     useEffect(() => {
-        document.title = `you clicked ${count} times`
-        console.log(count)
-    })
+        const time = setInterval(() => {
+            setCount( Count => Count + 1)
+        },1000)
+
+        return () => clearInterval(time)
+    },[])
     return (
         <div>
             <h2>Effect</h2>
-            <p>click {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
+            <p>🕐 {count} times</p>
+            <button onClick={() => setCount(count * 0)}>
+                Reset
             </button>
         </div>
     )
